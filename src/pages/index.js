14e85d496574313bb80387826e10c5f7ecea8b29
unknown <guilherme@pointmedia.com.br>
2022-04-25 13:44:42 -0300

    Primeiro push, configurando arquivos

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import styles from '../styles/index.module.css'
+
+export default function LandingPage() {
+
+  return (
+    <div className={styles.page}>
+      Olá mundo
+    </div>
+  )
+}
