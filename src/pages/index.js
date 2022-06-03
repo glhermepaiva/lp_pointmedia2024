4ebbc9681eb3051cd5ee6dfdb7720773cf6cae23
unknown <guilherme@pointmedia.com.br>
2022-06-03 18:57:35 -0300

    Corrigido link para fontes

--- src/pages/index.js
+++ src/pages/index.js
@@ -208,7 +208,7 @@ export default function LandingPage() {
       <Head>
         <title>POINT MEDIA</title>
         <meta name='description' content='Motion é o que realmente nos move' />
-        <link href='/public/fonts/fonts.css' rel="stylesheet" />
+        <Link href='/public/fonts/fonts.css' rel="stylesheet" />
       </Head>
       
       <div id='header' className={styles.header}>
