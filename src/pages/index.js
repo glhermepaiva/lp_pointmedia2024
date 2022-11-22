b92edf6cbf4e699c848486191f866b3ec5c78318
unknown <guilherme@pointmedia.com.br>
2022-11-22 16:12:22 -0300

    Removida Gabriela

--- src/pages/index.js
+++ src/pages/index.js
@@ -1762,11 +1762,11 @@ function parallaxAnimContact() {
             <div className={styles.MarianaName}>Mariana</div>
             <div className={styles.MarianaJob}>Head de Operações</div>
           </div>
-          <div className={styles.Gabriela}>
+          {/* <div className={styles.Gabriela}>
             <div className={styles.GabrielaPicture} />
             <div className={styles.GabrielaName}>Gabriela</div>
             <div className={styles.GabrielaJob}>Head de Atendimento de Mídia</div>
-          </div>
+          </div> */}
           <div className={styles.Louro}>
             <div className={styles.LouroPicture} />
             <div className={styles.LouroName}>Leonardo</div>
